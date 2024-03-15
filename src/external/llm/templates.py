cd66bfc137a8c6aedc814d8907a8f6003064ab2b
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-03-15 19:48:20 -0300

    feat: improve template separations

--- src/external/llm/templates.py
+++ src/external/llm/templates.py
@@ -1,8 +1,14 @@
 sql_template = """Write a SQL query that would answer the user's question,
 based on the context:
-{context}
 
-Question: {question}
+<context>
+{context}
+</context>
+
+<question>
+{question}
+</question>
+
 SQL Query:"""
 
 vega_spec_template = """Based on the context below, question,
@@ -11,7 +17,16 @@ sql query generate a vega_lite spec, json only no text or comments.
 - the vega_lite spec must not have the "data" key, only "mark" and "encoding".
 - the vega_lite spec must contain only fields present on the sql query.
 
+<context>
 {context}
+</context>
 
-Question: {question}
-SQL Query: {query}"""
+<question>
+{question}
+</question>
+
+<sql-query>
+{query}
+</sql-query>
+
+Vega-Lite Spec:"""
