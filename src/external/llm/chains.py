fb28f768bad3a30be569d1053da4869f438ade30
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-03-15 23:05:46 -0300

    feat: add openai functions

--- src/external/llm/chains.py
+++ src/external/llm/chains.py
@@ -1,12 +1,31 @@
 from typing import Any
 
 from langchain.chains.llm import LLMChain
-from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+from langchain.output_parsers.openai_functions import JsonOutputFunctionsParser
+from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.retrievers import BaseRetriever
 from langchain_core.runnables import RunnablePassthrough
 
 from src.external.llm.templates import sql_template, vega_spec_template
+
+functions = [
+    {
+        "name": "vega_spec",
+        "description": "Vega lite json chart spec",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "mark": {"type": "object", "description": "vega lite mark"},
+                "encoding": {
+                    "type": "object",
+                    "description": "vega lite encoding",
+                },
+            },
+            "required": ["mark", "encoding"],
+        },
+    }
+]
 
 
 def get_sql_chain(llm: Any, retriever: BaseRetriever) -> LLMChain:
@@ -32,6 +51,11 @@ def get_vega_chain(llm: Any, retriever: BaseRetriever) -> LLMChain:
             "question": RunnablePassthrough(),
         }
         | prompt
-        | llm.bind(stop=["\nVega-Lite Spec:"])
-        | JsonOutputParser()
+        | llm.bind(
+            stop=["\nVega-Lite Spec:"],
+            function_call={"name": "vega_spec"},
+            functions=functions,
+        )
+        | JsonOutputFunctionsParser()
+        # | JsonOutputParser()
     )
