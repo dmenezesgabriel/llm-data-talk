0667b3ece41eb7127eb969a7da14ef1cb2defe60
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-03-19 07:34:05 -0300

    feat: add cahcing to commands

--- app/src/core/commands/llm.py
+++ app/src/core/commands/llm.py
@@ -3,21 +3,31 @@ from typing import Any, Dict
 
 class LLMGetSQLCommand:
     def __init__(self, user_question: str, retriever, llm_controller) -> None:
-        self.user_question = user_question
-        self.retriever = retriever
-        self.llm_controller = llm_controller
+        self._user_question = user_question
+        self._retriever = retriever
+        self._llm_controller = llm_controller
+        self._cached_result = None
 
-    def execute(self) -> str:
-        return self.llm_controller.get_sql(self.user_question, self.retriever)
+    def execute(self, cached_result=True) -> str:
+        if cached_result and self._cached_result:
+            return self._cached_result
+        self._cached_result = self._llm_controller.get_sql(
+            self._user_question, self._retriever
+        )
+        return self._cached_result
 
 
 class LLMGetChartCommand:
     def __init__(self, user_question: str, retriever, llm_controller) -> None:
-        self.user_question = user_question
-        self.retriever = retriever
-        self.llm_controller = llm_controller
+        self._user_question = user_question
+        self._retriever = retriever
+        self._llm_controller = llm_controller
+        self._cached_result = None
 
-    def execute(self) -> Dict[str, Any]:
-        return self.llm_controller.get_chart(
-            self.user_question, self.retriever
+    def execute(self, cached_result=True) -> Dict[str, Any]:
+        if cached_result and self._cached_result:
+            return self._cached_result
+        self._cached_result = self._llm_controller.get_chart(
+            self._user_question, self._retriever
         )
+        return self._cached_result
