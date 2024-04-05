3f4b0ffd0821ff6ba7bfb6265db4310dc98a7065
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-04-05 18:46:48 -0300

    refactor: template formatting

--- app/src/external/llm/langchain/templates.py
+++ app/src/external/llm/langchain/templates.py
@@ -2,92 +2,104 @@ from textwrap import dedent
 
 intent_extraction_template = dedent(
     """
-    You need to extract intents from the user question in specified format.\n
-    Extracted intents always should have valid json format, if you don't\n
-    find any intents then respond with empty list.
+You need to extract intents from the user question in specified format.
+Extracted intents always should have valid json format, if you don't \
+find any intents then respond with empty list.
 
-    Intent Format: List of dict, where each dict having format as:\n
-    {{
-        "intent":"{{question intent}}",
-        "entity":"{{
-            "entity_type": {{question intent target entity type}},
-            "entity_name": {{question intent target entity name}},
-            }}",
-        "action":"{{
-            "action_subject": {{question intent action subject}},
-            "action_details": {{question intent action details}},
-            }}",
-    }}
+Intent Format: List of dict, where each dict having format as:
 
-    <question>
-    {question}
-    </question>
+{{
+    "intent":"{{question intent}}",
+    "entity":"{{
+        "entity_type": {{question intent target entity type}},
+        "entity_name": {{question intent target entity name}},
+        }}",
+    "action":"{{
+        "action_subject": {{question intent action subject}},
+        "action_details": {{question intent action details}},
+        }}",
+}}
 
-    Intents:
-    """
+<question>
+{question}
+</question>
+
+Intents:
+"""
 )
 
 sql_template = dedent(
     """
-    Write a SQL query that would answer the user's question, based on the\n
-    context below.\n
-    The queries should never use table name abbreviations, e.g. "t" instead\n
-    of "table".\n
+Write a SQL query that would answer the user's question, based on the \
+context below.
+The queries should never use table name abbreviations, e.g. "t" instead \
+of "table".
 
-    <context>
-    {context}
-    </context>
+<context>
+{context}
+</context>
 
-    <question>
-    {question}
-    </question>
+<question>
+{question}
+</question>
 
-    SQL Query:
-    """
+SQL Query:
+"""
 )
 
 sql_entity_extraction_template = dedent(
     """
-    You need to extract entities from the user query `WHERE` and `HAVING`
-    clauses in specified format.\n
-    Extracted entities always should have valid json format, if you don't\n
-    find any entities then respond with empty list.
+You need to extract entities from the user query `WHERE` and `HAVING` \
+clauses in specified format.
+Extracted entities always should have valid json format, if you don't \
+find any entities then respond with empty list.
 
-    Entity Format: List of dict, where each dict having format as:\n
-    {{
-        "entity":"{{entity key}}",
-        "attribute":"{{entity attribute key}}",
-        "operator":"{{valid sql operator}}",
-        "value":"{{value of entity}}"
-    }}
+Entity Format: List of dict, where each dict having format as:
 
-    <sql-query>
-    {query}
-    </sql-query>
+{{
+    "entity":"{{entity key}}",
+    "attribute":"{{entity attribute key}}",
+    "operator":"{{valid sql operator}}",
+    "value":"{{value of entity}}"
+}}
 
-    <question>
-    {question}
-    </question>
+<sql-query>
+{query}
+</sql-query>
 
-    Entities:
-    """
+<question>
+{question}
+</question>
+
+Entities:
+"""
 )
 
 chart_template = dedent(
     """
-    Your task is to generate chart configuration for the given dataset and user
-    question.\n
-    Responses should be in JSON format compliant with the vega-lite
-    specification, but `data` field must be excluded.\n
+Your task is to generate chart configuration for the given dataset and user \
+question.
+Responses should be in JSON format compliant with the vega-lite \
+specification, but `data` field must be excluded.
 
-    <question>
-    {question}
-    </question>
+<question>
+{question}
+</question>
 
-    <dataset>
-    {schema}
-    </dataset>
+<dataset>
+{schema}
+</dataset>
 
-    Json:
-    """
+Json:
+"""
 )
+
+if __name__ == "__main__":
+    print(20 * "=")
+    print(intent_extraction_template)
+    print(20 * "=")
+    print(sql_template)
+    print(20 * "=")
+    print(sql_entity_extraction_template)
+    print(20 * "=")
+    print(chart_template)
