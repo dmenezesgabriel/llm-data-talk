6977f5f4f30ed4e546de06aaaf7bb1323952996a
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-04-06 17:06:29 -0300

    feat: add analytics enginieering planner

---
+++ app/src/external/llm/langchain/chains/analytics_engineering_planner.py
@@ -0,0 +1,69 @@
+from operator import itemgetter
+from typing import Any
+
+from langchain.chains.llm import LLMChain
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.retrievers import BaseRetriever
+from src.common.utils.performance import log_time
+from src.external.llm.langchain.chains.base import BaseChain
+from src.external.llm.langchain.templates import (
+    ANALYTICS_ENGINING_PLANNER_TEMPLATE,
+)
+
+
+class AnalyticsEngineeringPlannerChain(BaseChain):
+    def __init__(self, llm: Any, retriever: BaseRetriever) -> None:
+        super().__init__(llm, retriever)
+
+    @log_time
+    def chain(self) -> LLMChain:
+        prompt = PromptTemplate(
+            template=ANALYTICS_ENGINING_PLANNER_TEMPLATE,
+            input_variables=["context", "question"],
+        )
+
+        return (
+            {
+                "context": itemgetter("question") | self._retriever,
+                "question": itemgetter("question"),
+            }
+            | prompt
+            | self._llm
+            | JsonOutputParser()
+        )
+
+
+if __name__ == "__main__":
+    from langchain_community.vectorstores import Chroma
+    from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+    from src.common.utils.database import get_database_connection
+    from src.config import get_config
+    from src.external.llm.langchain.helpers.text import TextHelper
+
+    conn = get_database_connection()
+    with open("./data/schema.sql") as f:
+        schema = f.read()
+
+    config = get_config()
+    text_chunks = TextHelper.get_text_chunks(schema)
+    llm = ChatOpenAI(api_key=config.OPENAI_API_KEY)
+    embeddings = OpenAIEmbeddings(api_key=config.OPENAI_API_KEY)
+    vector_store = Chroma.from_texts(texts=text_chunks, embedding=embeddings)
+    retriever = vector_store.as_retriever()
+
+    # ======================================================================= #
+    sql_chain = AnalyticsEngineeringPlannerChain(llm=llm, retriever=retriever)
+    for question in [
+        "what are the top 20 artists by sales?",
+        "Plot the top 10 artists by sales",
+        "What is the artist with most sales?",
+        "What is the artist with the most sales and the one with less sales? "
+        "Also give me the difference in percentage of sales between the two",
+    ]:
+        sql_chain_result = sql_chain.chain().invoke(
+            input={"question": question}
+        )
+        print(50 * "=")
+        print(sql_chain_result)
+        print(50 * "=")
--- app/src/external/llm/langchain/models.py
+++ app/src/external/llm/langchain/models.py
@@ -1,5 +1,5 @@
 from textwrap import dedent
-from typing import Literal
+from typing import List, Literal
 
 from langchain_core.pydantic_v1 import BaseModel, Field
 
@@ -16,3 +16,14 @@ class ResponseTypeRouteQuery(BaseModel):
             ),
         )
     )
+
+
+class AnalyticsEngineeringTaskPlan(BaseModel):
+    question: str = Field(..., description="Business question to be answered")
+    final_response_format: Literal["chart", "text", "table"] = Field(
+        ..., description="Task response format"
+    )
+
+
+class AnalyticsEngineeringTaskPlanList(BaseModel):
+    tasks: List[AnalyticsEngineeringTaskPlan]
