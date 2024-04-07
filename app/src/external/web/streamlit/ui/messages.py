b37713a0474754032f49a5a1c17fa3f9d285501f
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-04-07 17:05:59 -0300

    feat: add dynamic filters

--- app/src/external/web/streamlit/ui/messages.py
+++ app/src/external/web/streamlit/ui/messages.py
@@ -4,6 +4,9 @@ import pandas as pd
 import streamlit as st
 from src.common.utils.database import get_database_connection
 from src.config import get_config
+from src.external.web.streamlit.ui.dynamic_dataframe_filters import (
+    dynamic_dataframe_filter,
+)
 
 config = get_config()
 
@@ -20,23 +23,26 @@ def format_assistant_message(message_content) -> None:
     sql = chart_spec["sql_query"]
     df = pd.read_sql_query(sql, conn)
 
-    tab_titles = ["SQL", "Table", "Chart", "Text"]
-    tab_sql, tab_table, tab_chart, tab_text = st.tabs(tab_titles)
+    tab_titles = ["SQL", "Table", "Chart", "Text", "Steps"]
+    tab_sql, tab_table, tab_chart, tab_text, tab_steps = st.tabs(tab_titles)
 
     with tab_sql:
         st.code(sql, language="sql")
 
     with tab_table:
-        st.dataframe(df)
+        dynamic_filters_table = dynamic_dataframe_filter(df, key="table")
+        st.dataframe(dynamic_filters_table)
 
     with tab_chart:
-        with st.expander("spec"):
-            st.write(chart_spec)
-
-        st.vega_lite_chart(df, chart)
+        dynamic_filters_table_chart = dynamic_dataframe_filter(df, key="chart")
+        st.vega_lite_chart(dynamic_filters_table_chart, chart)
 
     with tab_text:
         st.write("Not Implemented")
+
+    with tab_steps:
+        with st.expander("spec"):
+            st.write(chart_spec)
 
 
 def message_formatter_factory(role: str) -> Callable:
